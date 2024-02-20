b73782215d4a2adc7d3eb92d685958e1d05d89c2
Collin <collin.press@gmail.com>
2024-02-20 12:26:06 +0700

    i didn't save the code lol

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-import { Chart, registerables } from "chart.js";
-import adapterDateFns from "chartjs-adapter-date-fns";
-
 
 document.addEventListener('DOMContentLoaded', () => {
 
